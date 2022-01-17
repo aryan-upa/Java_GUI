5f54f6a149c7320ad374a2f03b2e282ff4f7d3f5
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-17 23:26:46 +0530

    create the FirstApp.java file, added the main method and other functionalities. pretty basic.

--- src/AWT_Based/FirstApp/FirstApp.java
+++ src/AWT_Based/FirstApp/FirstApp.java
@@ -3,5 +3,20 @@ package AWT_Based.FirstApp;
 import java.awt.*;
 
 public class FirstApp {
+    public static void main(String[] args) {
+        Frame f = new Frame("First APP");
+        f.setLayout(new FlowLayout()); // Layout.
+
+        Button b = new Button("A button");
+        Label l = new Label("Name");
+        TextField tf = new TextField(20);
+
+        f.add(l); //add a label.
+        f.add(b); //adding the button on the screen.
+        f.add(tf); //add the text field.
+
+        f.setSize(300,300); //sets the size in pixels.
+        f.setVisible(true); // shows the frame.
+    }
 
 }
