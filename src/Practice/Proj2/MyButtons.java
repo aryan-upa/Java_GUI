1773aae3b08d170ee6b1bcc8676a683e1be532e4
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-29 21:02:38 +0530

    created MyButtons.java class and defined all the Buttons there, also created MyButtons.getAllButtons method to get a array of all the buttons in the project and also created MyButtons.setPropertyOfButtons method to set the property of all buttons at once.

---
+++ src/Practice/Proj2/MyButtons.java
@@ -0,0 +1,36 @@
+package Practice.Proj2;
+
+import java.awt.*;
+
+public class MyButtons extends Button {
+
+    Button fact, isP, isA, rand, sum, sub, mul, div, solve;
+    Button[] allButtons;
+
+    {
+        fact = new Button("Factorial");
+        isP = new Button("isPrime");
+        isA = new Button("isArmstrgetong");
+        rand = new Button("Random");
+        sum = new Button("Addition");
+        sub = new Button("Subtract");
+        mul = new Button("Multiply");
+        div = new Button("Divide");
+        solve = new Button("Solve = ");
+
+        allButtons = new Button[]{fact, sum, isA, sub, isP, mul, rand, div};
+    }
+
+    MyButtons() {
+    }
+
+    public Button[] getAllButtons() {
+        return this.allButtons;
+    }
+
+    public void setPropertyOfButtons(Color c) {
+        for (Button button: allButtons) {
+            button.setBackground(c);
+        }
+    }
+}
